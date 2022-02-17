3814860cbb9643adb387885dd86582af0361b746
AnSaScience <ansascience@gmail.com>
2022-02-17 21:04:15 +0200

    init: start task towelSort; feat: made task towelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix===undefined || matrix.length===0  )
+  {
+    return []
+  }
+ let newArray=matrix.map( (x,i) => i%2===0 ? (x.sort((a,b)=>a-b)).join(" ") : x.sort((a,b)=>b-a).join(" ")).join(" ").split(" ").map( x => +x)
+  return (newArray)
 }
